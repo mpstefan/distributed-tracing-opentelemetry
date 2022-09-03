c9a2f0ab3900e29fa957b5f0d490f728ab59ca24
Mike Stefaniak <mcledin@yahoo.com>
2022-09-03 16:33:52 -0400

    Logic in random gateway added, no API calls yet.

--- java-src/random-gateway/src/main/java/com/randomGateway/randomGateway/GatewayController.java
+++ java-src/random-gateway/src/main/java/com/randomGateway/randomGateway/GatewayController.java
@@ -3,11 +3,26 @@ package com.randomGateway.randomGateway;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+import java.util.random.RandomGenerator;
+
 @RestController
 public class GatewayController {
-
+    Random random = new Random();
+    final int maxCharactersToReturn = 15;
     @GetMapping("/randomString")
     public String generateRandomString(){
-        return "This is absolutely not random";
+        int charactersToReturn = random.nextInt(maxCharactersToReturn) + 1;
+        String somewhatRandomString = "";
+
+        for(int i = 0; i < charactersToReturn; i++){
+            int coinflip = random.nextInt(2);
+            if(coinflip == 0){
+                somewhatRandomString += "1";
+            }else{
+                somewhatRandomString += "L";
+            }
+        }
+        return somewhatRandomString;
     }
 }
--- java-src/random-gateway/src/main/java/com/randomGateway/randomGateway/RandomGatewayApplication.java
+++ java-src/random-gateway/src/main/java/com/randomGateway/randomGateway/RandomGatewayApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RandomGatewayApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Yo world");
 		SpringApplication.run(RandomGatewayApplication.class, args);
 	}
 
