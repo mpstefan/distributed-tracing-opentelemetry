cb9d0290074410108eda29f04501cf623eff070c
Mike Stefaniak <mcledin@yahoo.com>
2022-09-03 16:24:39 -0400

    Random Number API

--- java-src/random-numbers/src/main/java/com/randomNumbers/randomNumbers/RandomNumberController.java
+++ java-src/random-numbers/src/main/java/com/randomNumbers/randomNumbers/RandomNumberController.java
@@ -1,13 +1,13 @@
-package com.randomLetters.randomLetters;
+package com.randomNumbers.randomNumbers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class RandomLetterController {
+public class RandomNumberController {
 
-    @GetMapping("/randomLetter")
+    @GetMapping("/randomNumber")
     public String generateRandomString(){
-        return "L";
+        return "1";
     }
 }
