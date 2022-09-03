01eaf60478a09ee544684f5aa7ced5168843872f
Mike Stefaniak <mcledin@yahoo.com>
2022-09-03 18:06:03 -0400

    Actual random numbers

--- java-src/random-numbers/src/main/java/com/randomNumbers/randomNumbers/RandomNumberController.java
+++ java-src/random-numbers/src/main/java/com/randomNumbers/randomNumbers/RandomNumberController.java
@@ -3,11 +3,15 @@ package com.randomNumbers.randomNumbers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
 @RestController
 public class RandomNumberController {
 
+    Random random = new Random();
     @GetMapping("/randomNumber")
     public String generateRandomString(){
-        return "1";
+
+        return Integer.toString(random.nextInt(10));
     }
 }
