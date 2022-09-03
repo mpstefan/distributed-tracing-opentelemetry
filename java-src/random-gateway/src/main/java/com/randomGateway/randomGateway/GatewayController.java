70c514a28ab1699555c414a2e8a993b51f909688
Mike Stefaniak <mcledin@yahoo.com>
2022-09-03 18:01:49 -0400

    API calls to letters and numbers, letters actual randomized

--- java-src/random-gateway/src/main/java/com/randomGateway/randomGateway/GatewayController.java
+++ java-src/random-gateway/src/main/java/com/randomGateway/randomGateway/GatewayController.java
@@ -1,28 +1,53 @@
 package com.randomGateway.randomGateway;
 
+import org.apache.coyote.Request;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Random;
-import java.util.random.RandomGenerator;
 
-@RestController
+@RestController @Service
 public class GatewayController {
     Random random = new Random();
     final int maxCharactersToReturn = 15;
+
+    WebClient randomLettersClient = WebClient.create("http://localhost:8081");
+    WebClient.UriSpec<WebClient.RequestBodySpec> letterUriSpec = (WebClient.UriSpec<WebClient.RequestBodySpec>) randomLettersClient.get();
+    WebClient.RequestBodySpec letterBodySpec = letterUriSpec.uri("/randomLetter");
+
+    WebClient randomNumbersClient = WebClient.create("http://localhost:8082");
+    WebClient.UriSpec<WebClient.RequestBodySpec> numberUriSpec = (WebClient.UriSpec<WebClient.RequestBodySpec>) randomNumbersClient.get();
+    WebClient.RequestBodySpec numberBodySpec = numberUriSpec.uri("/randomNumber");
+
+
+
     @GetMapping("/randomString")
     public String generateRandomString(){
         int charactersToReturn = random.nextInt(maxCharactersToReturn) + 1;
         String somewhatRandomString = "";
 
+
+
         for(int i = 0; i < charactersToReturn; i++){
-            int coinflip = random.nextInt(2);
-            if(coinflip == 0){
-                somewhatRandomString += "1";
+            int coinFlip = random.nextInt(2);
+            if(coinFlip == 0){
+                somewhatRandomString += getRandomLetter();
             }else{
-                somewhatRandomString += "L";
+                somewhatRandomString += getRandomNumber();
             }
         }
         return somewhatRandomString;
+    }
+
+    public String getRandomLetter(){
+        WebClient.ResponseSpec apiResponse = letterBodySpec.retrieve();
+        return apiResponse.bodyToMono(String.class).block();
+    }
+
+    public String getRandomNumber(){
+        WebClient.ResponseSpec apiResponse = numberBodySpec.retrieve();
+        return apiResponse.bodyToMono(String.class).block();
     }
 }
--- java-src/random-letters/src/main/java/com/randomLetters/randomLetters/RandomLetterController.java
+++ java-src/random-letters/src/main/java/com/randomLetters/randomLetters/RandomLetterController.java
@@ -2,12 +2,17 @@ package com.randomLetters.randomLetters;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Random;
 
 @RestController
 public class RandomLetterController {
 
+    String[] allLetters = new String[]{"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+    Random random = new Random();
+
     @GetMapping("/randomLetter")
     public String generateRandomString(){
-        return "L";
+        int indexOfLetterToReturn = random.nextInt(52);
+        return allLetters[indexOfLetterToReturn];
     }
 }
